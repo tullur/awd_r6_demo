3c6ec7d2a3360a6e5c007b2249ffce351fa241ef
psidae <akson.rill@gmail.com>
2021-01-22 15:23:02 +0200

    Button

--- app/models/product.rb
+++ app/models/product.rb
@@ -8,4 +8,16 @@ class Product < ApplicationRecord
   }
 
   validates :price, numericality: { greater_than_or_equal_to: 0.01 }
+
+  has_many :line_items
+  before_destroy :ensure_not_referenced_by_any_line_item
+
+  private
+
+  def ensure_not_referenced_by_any_line_item
+    unless line_items.empty?
+      errors.add(:base, 'Line items present')
+      throw :abort
+    end
+  end
 end
